d2e93ef39cc83523495626ab1bb7226120074aa4
tozpeak <tozpeak@gmail.com>
2024-03-23 23:28:16 +0200

    implemented simple paddle

--- lib/gameplay_page.dart
+++ lib/gameplay_page.dart
@@ -19,6 +19,8 @@ class GameplayPage extends StatelessWidget {
           ),
           const AspectRatio(
             aspectRatio: GameplayContent.gameWidth / GameplayContent.gameHeight,
+            //width: 360,
+            //height: 800,
             child: GameplayContent(),
           ),
           Align(
@@ -48,8 +50,56 @@ class GameplayContent extends StatefulWidget {
 }
 
 class _GameplayContentState extends State<GameplayContent> {
+  double _paddleCenterPosition = GameplayContent.gameWidth / 2;
+
+  final _paddleSize = Size(60, 10);
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        final size = constrains.biggest;
+        final ratio = size.width / GameplayContent.gameWidth;
+        return GestureDetector(
+          onPanUpdate: (details) => setState(() {
+            _paddleCenterPosition = details.localPosition.dx / ratio;
+          }),
+          child: Container(
+            color: Colors.white24,
+            child: Stack(
+              //TODO: use CustomMultiChildLayout instead
+              children: [
+                SizedBox.expand(
+                  child: Align(
+                    alignment: Alignment(
+                      (
+                        2 * _paddleCenterPosition
+                        / (GameplayContent.gameWidth-_paddleSize.width)
+                      ) 
+                      - 1
+                      - _paddleSize.width / (GameplayContent.gameWidth - _paddleSize.width), 
+                      0.8
+                    ),
+                    child: SizedBox.fromSize(
+                      size: _paddleSize * ratio,
+                      child: Container(color: Colors.green,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: _paddleSize.width * ratio / 2 - 1),
+                          child: Container(
+                            color: Colors.black
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
   }
 }
