3b5c083e0960b99f27da63bf77b7907d1f6cedeb
tozpeak <tozpeak@gmail.com>
2024-03-24 02:48:29 +0200

    first gameplay loop working (physics is buggy)

--- lib/custom_physics.dart
+++ lib/custom_physics.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 
 class CustomPhysics {
 
-  static Offset? collisionSphereToBox(Rect sphere, Rect box, double value) {
+  static Offset? collisionSphereToBox(Rect sphere, Rect box) {
+    if(!sphere.overlaps(box)) return null;
     var aabbIntersection = sphere.intersect(box);
-    if(aabbIntersection.isEmpty) return null;
 
     final List<Offset> withinRadius = [
       aabbIntersection.topLeft,
@@ -13,16 +13,47 @@ class CustomPhysics {
       aabbIntersection.bottomRight,
     ];
     Offset center = sphere.center;
-    double radius = sphere.width / 2;
+    //double radius = sphere.width / 2;
 
-    withinRadius.removeWhere((element) => !isWithinRadius(element, center, radius));
+    for (var i = withinRadius.length-1; i > 0; i--) {
+      for (var j = 0; j < i; j++) {
+        if(withinRadius[j] == withinRadius[i]) {
+          withinRadius.removeAt(i);
+          break;
+        }
+      }
+    }
+    //print(withinRadius);
+
+    if(withinRadius.length % 2 == 0) {
+      Offset expected = withinRadius.fold(
+        Offset.zero, 
+        (previousValue, element) => previousValue + element / (withinRadius.length * 1.0)
+      );
+      withinRadius.clear();
+      withinRadius.add(expected);
+    }
+
+    //withinRadius.removeWhere((element) => !isWithinRadius(element, center, radius));
+    //print(withinRadius);
 
     if(withinRadius.isEmpty) return null;
 
-    Offset normal = withinRadius.fold(
-      Offset.zero, 
-      (previousValue, element) => previousValue + (center - element)
+    Offset target = withinRadius.fold(
+      Offset.infinite, 
+      (previousValue, element) => 
+        ((previousValue - center).distance < (element - center).distance) 
+        ? previousValue
+        : element
     );
+
+    Offset normal = center - target;
+
+    // print({
+    //   "target": target,
+    //   "center": center,
+    //   "normal": normal
+    // });
 
     return Offset.fromDirection(normal.direction);
   }
--- lib/gameplay_page.dart
+++ lib/gameplay_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo_bricks/custom_physics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -49,11 +50,129 @@ class GameplayContent extends StatefulWidget {
   State<GameplayContent> createState() => _GameplayContentState();
 }
 
-class _GameplayContentState extends State<GameplayContent> {
-  double _paddleCenterPosition = GameplayContent.gameWidth / 2;
+class _GameplayContentState extends State<GameplayContent> 
+    with SingleTickerProviderStateMixin {
+  
+  late AnimationController _controller;
+  bool _isGameOver = false;
 
-  final _paddleSize = const Size(60, 10);
+  Offset _paddleCenterPosition = const Offset(
+    GameplayContent.gameWidth / 2, 
+    GameplayContent.gameHeight * 0.9
+  );
 
+  final _paddleSize = const Size(60, 20);
+
+  Rect ball = Rect.fromCircle(
+    center: const Offset(GameplayContent.gameWidth / 2, GameplayContent.gameHeight * 0.8),
+    radius: 10,
+  );
+  Offset ballSpeed = Offset.fromDirection(-1, 280);
+    
+  final walls = [
+    Rect.fromPoints(const Offset(-10, -10), const Offset(0, GameplayContent.gameHeight)),
+    Rect.fromPoints(const Offset(-10, -10), const Offset(GameplayContent.gameWidth, 0)),
+    Rect.fromPoints(
+      const Offset(GameplayContent.gameWidth, -10), 
+      const Offset(GameplayContent.gameWidth, GameplayContent.gameHeight)
+    ),
+    
+  ];
+
+  Rect gameOverCollider = Rect.fromPoints(
+    const Offset(-10, GameplayContent.gameHeight * 0.95), 
+    const Offset(GameplayContent.gameWidth+10, GameplayContent.gameHeight)
+  );
+
+  final targets = <Rect>[];
+  
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+
+    _controller.addListener(() {
+      setState(() {
+        _physicsUpdate(0.02);
+      });
+    });
+
+    const rows = 10;
+    const colums = 5;
+    const margin = 10.0;
+    const blockSize = Size(60, 20);
+    const startPos = Offset(40.0, GameplayContent.gameHeight * 0.1);
+    final dx = Offset(blockSize.width + margin, 0);
+    final dy = Offset(0, blockSize.height + margin);
+
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < colums; j++) {
+        targets.add(Rect.fromCenter(
+          center: startPos + dx * (j * 1.0) + dy * (i * 1.0), 
+          width: blockSize.width, 
+          height: blockSize.height
+        ));
+      }
+    }
+  }
+
+  void _startAnimation() {
+    _controller.repeat(min: 0, max: 1, period: Durations.short1);
+  }
+
+  @override 
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  FractionalOffset _getViewportOffset(Offset ingameCenter, Size size) {
+    return FractionalOffset(
+      (ingameCenter.dx - size.width / 2) 
+      / 
+      (GameplayContent.gameWidth - size.width)
+      , 
+      (ingameCenter.dy - size.height / 2) 
+      / 
+      (GameplayContent.gameHeight - size.height)
+    );
+  }
+
+  _physicsUpdate(double dTime) {
+    if(_isGameOver) return;
+
+    ball = ball.translate(ballSpeed.dx * dTime, ballSpeed.dy * dTime);
+    //print(ball.center);
+
+    for (var wall in walls) {
+      _collideBallWith(wall);
+    }
+    _collideBallWith(Rect.fromCenter(
+      center: _paddleCenterPosition, 
+      width: _paddleSize.width, 
+      height: _paddleSize.height,
+    ));
+
+    if(_collideBallWith(gameOverCollider)) {
+      print("Game Over");
+      ballSpeed = Offset.zero;
+      _isGameOver = true;
+    }
+
+    for (var i = 0; i < targets.length; i++) {
+      if(_collideBallWith(targets[i])) {
+        targets.removeAt(i);
+        i--;
+      }
+    }
+  }
+
+  bool _collideBallWith(Rect rect) {
+    var normal = CustomPhysics.collisionSphereToBox(ball, rect);
+    if(normal == null) return false;
+    ballSpeed = CustomPhysics.reflectSpeed(ballSpeed, normal);
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,8 +182,17 @@ class _GameplayContentState extends State<GameplayContent> {
         final size = constrains.biggest;
         final ratio = size.width / GameplayContent.gameWidth;
         return GestureDetector(
+          onPanDown: (detailds) {
+            if(!_controller.isAnimating) {
+              _startAnimation();
+            }
+          },
           onPanUpdate: (details) => setState(() {
-            _paddleCenterPosition = details.localPosition.dx / ratio;
+            _paddleCenterPosition = Offset(
+              details.localPosition.dx / ratio, 
+              _paddleCenterPosition.dy
+            );
+            //_physicsUpdate(0.3);
           }),
           child: Container(
             color: Colors.white24,
@@ -73,22 +201,41 @@ class _GameplayContentState extends State<GameplayContent> {
               children: [
                 SizedBox.expand(
                   child: Align(
-                    alignment: FractionalOffset(
-                      (_paddleCenterPosition - _paddleSize.width / 2) 
-                      / 
-                      (GameplayContent.gameWidth-_paddleSize.width)
-                      , 
-                      0.9
-                    ),
+                    alignment: _getViewportOffset(_paddleCenterPosition, _paddleSize),
                     child: SizedBox.fromSize(
                       size: _paddleSize * ratio,
                       child: Container(color: Colors.green,
                         child: Padding(
                           padding: EdgeInsets.symmetric(horizontal: _paddleSize.width * ratio / 2 - 1),
                           child: Container(
-                            color: Colors.black
+                            color: Colors.black,
                           ),
                         ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox.expand(
+                  child: Align(
+                    alignment: _getViewportOffset(ball.center, ball.size),
+                    child: SizedBox.fromSize(
+                      size: ball.size * ratio,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ...targets.map<Widget>((target) => 
+                  SizedBox.expand(
+                    child: Align(
+                      alignment: _getViewportOffset(target.center, target.size),
+                      child: SizedBox.fromSize(
+                        size: target.size * ratio,
+                        child: Container(color: Colors.blue[200]),
                       ),
                     ),
                   ),
@@ -100,4 +247,4 @@ class _GameplayContentState extends State<GameplayContent> {
       }
     );
   }
-}
+} 
